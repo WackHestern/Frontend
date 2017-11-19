e6f5208f51b18c5246cfc286f5b0d017cbbb65de
Jonathan Sun <Jspsun@gmail.com>
2017-11-18 20:00:38 -0500

    add signin

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import Home from './components/views/Home/home';
 import SignUp from './components/views/SignUp/signup'
-import signin from './components/views/SignIn/signin'
+import signin from './components/views/SignIn/signIn'
 
 export default (
   <Route path='/' component={App}>
