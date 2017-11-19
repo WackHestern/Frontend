bfdac9d9a98f984377304be963c3e3ff0fdcf61e
Jonathan Sun <Jspsun@gmail.com>
2017-11-19 09:07:06 -0500

    a

--- src/components/views/Admin/Components/AlgoBalance/AlgoBalance.jsx
+++ src/components/views/Admin/Components/AlgoBalance/AlgoBalance.jsx
@@ -15,7 +15,6 @@ export class AlgoBalance extends Component {
 
   constructor() {
     super();
-    console.log(this.props)
     this.state ={
       history: <i className="fa fa-cog fa-spin fa-3x fa-fw" ></i>,
       toRefresh: true
@@ -23,22 +22,24 @@ export class AlgoBalance extends Component {
     this.update=this.update.bind(this)
     this.plotgraph=this.plotgraph.bind(this)
     this.timeout;
+    this.toRefresh=true;
   }
 
   update (){
-    if(this.state.toRefresh){
+    console.log(this.props.securities, this.props.principle, String(this.props.strategy))
+    if(this.toRefresh){
       return axios({
         method:'post',
         url: proxyurl + 'https://momentumtrader.herokuapp.com/Tradium/projection',
         data: {
           securities: this.props.securities,
-          start_cash: parseInt(this.props.principle),
-          strategy: this.props.strategy,
+          // start_cash: parseInt(this.props.principle),
+          start_cash: 100000,
+          strategy: String(this.props.strategy),
         }
       })
         .then((res) => {
           const data = res.data.networth_over_time
-          console.log(data)
 
           let days=[]
           for (let i=0;i<100;i++){
@@ -48,10 +49,13 @@ export class AlgoBalance extends Component {
           return[days, data];
       })
       .then(([x, y]) => {
-        this.setState({history: this.plotgraph(x,y), toRefresh:false})
+        this.toRefresh=false
+        this.setState({history: this.plotgraph(x,y), toRefresh:true})
       })
+    }else {
+      this.toRefresh=true;
     }
-  }
+  } 
 
   plotgraph(xdata, ydata){
     let data = [
@@ -112,7 +116,6 @@ export class AlgoBalance extends Component {
   // }
 
   render() {
-    console.log('rerendered')
     this.update();
     return (
       <div className="AlgoBalance">
