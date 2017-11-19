dd7246510b8de77422f6c6035d33d6561758b4c6
Jonathan Sun <Jspsun@gmail.com>
2017-11-19 05:16:58 -0500

    add graph

--- src/components/views/Portfolio/Components/PortfolioBalance/PortfolioBalance.jsx
+++ src/components/views/Portfolio/Components/PortfolioBalance/PortfolioBalance.jsx
@@ -4,6 +4,10 @@ import {Button} from '../../../Common/Button/Button.jsx'
 
 const _ = require('lodash');
 const axios = require('axios');
+import createPlotlyComponent from 'react-plotlyjs';
+//See the list of possible plotly bundles at https://github.com/plotly/plotly.js/blob/master/dist/README.md#partial-bundles or roll your own
+import Plotly from 'plotly.js/dist/plotly-cartesian';
+const PlotlyComponent = createPlotlyComponent(Plotly);
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
@@ -12,10 +16,11 @@ export class PortfolioBalance extends Component {
   constructor() {
     super();
     this.state ={
-      history: ''
+      history: <i className="fa fa-cog fa-spin fa-3x fa-fw" ></i>
     }
     this.reRender=this.reRender.bind(this)
     this.update=this.update.bind(this)
+    this.plotgraph=this.plotgraph.bind(this)
     this.timeout;
     this.reRender()
   }
@@ -27,6 +32,37 @@ export class PortfolioBalance extends Component {
   }
 
   update (){
+  }
+
+  plotgraph(xdata, ydata){
+    let data = [
+      {
+        type: 'scatter',  // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
+        x: xdata,     // more about "x": #scatter-x
+        y: ydata,     // #scatter-y
+        marker: {         // marker is an object, valid marker keys: #scatter-marker
+          color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
+        }
+      },
+    ];
+    let layout = {                     // all "layout" attributes: #layout   // more about "layout.title": #layout-title
+      xaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
+        title: 'Date'         // more about "layout.xaxis.title": #layout-xaxis-title
+      },
+      yaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
+      title: 'Value'         // more about "layout.xaxis.title": #layout-xaxis-title
+    },
+      annotations: [            // all "annotation" attributes: #layout-annotations
+      ]
+    };
+    let config = {
+      showLink: false,
+      displayModeBar: false
+    };
+    console.log('aaaaa')
+    return (
+      <PlotlyComponent className="whatever" data={data} layout={layout} config={config}/>
+    );
   }
 
   componentDidMount(){
@@ -42,22 +78,24 @@ export class PortfolioBalance extends Component {
         _.forEach(Object.keys(data), (day) => {
           days.push(day)
           let total = 0
-          _.forEach(stock, () => {
+          _.forEach(data[day], (stock) => {
             total += Object.values(stock)[0]
           });
           values.push(total)
         });
 
-        console.log(days, values)
-
-        this.setState({history: res.data.message})
-    });
+        return[days, values]
+    })
+    .then(([x, y]) => {
+      this.setState({history: this.plotgraph(x,y)})
+    })
   }
 
   render() {
     return (
       <div className="PortfolioBalance">
         <div className="headerboi">Portfolio Balance History</div>
+        {this.state.history}
       </div>
     );
   }
